7778197bb85e796cef0eae8f962cd08842aede57
Ivan Almer <ivan.almer@fer.hr>
2018-12-22 17:12:21 +0000

    Spohanac

--- MyApplication/app/src/main/java/com/example/lovro/myapplication/activities/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/lovro/myapplication/activities/LoginActivity.java
@@ -1,22 +1,45 @@
 package com.example.lovro.myapplication.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.lovro.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Variable definition
+    Button google_login;
+    Button email_login;
+    TextView sign_up;
+    TextView terms_of_service;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //variable initialisation
+        google_login = findViewById(R.id.google_login);
+        email_login = findViewById(R.id.email_login);
+        sign_up = findViewById(R.id.sign_in);
+        terms_of_service = findViewById(R.id.terms);
+
+        //listeners initialisation
+        initListeners();
     }
 
-    private boolean login(String email, String pass){
-        return true;
+    private void initListeners(){
+        email_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register_activity = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(register_activity);
+            }
+        });
     }
-
-    private void forgotPassword(){}
 }
--- MyApplication/app/src/main/java/com/example/lovro/myapplication/activities/RegisterActivity.java
+++ MyApplication/app/src/main/java/com/example/lovro/myapplication/activities/RegisterActivity.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.lovro.myapplication.R;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
     }
-
 
     private boolean register(String email, String pass, String repeatedpass){
         return true;
