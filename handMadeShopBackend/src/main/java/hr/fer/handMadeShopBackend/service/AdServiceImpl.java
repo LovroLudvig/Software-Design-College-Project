9ee5ebb958e2216876a53752034869cff17239a7
Martin Čolja <martin.colja@fer.hr>
2019-01-04 16:04:19 +0100

    -dodao provjeru podataka kod adova

--- handMadeShopBackend/src/main/java/hr/fer/handMadeShopBackend/service/AdServiceImpl.java
+++ handMadeShopBackend/src/main/java/hr/fer/handMadeShopBackend/service/AdServiceImpl.java
@@ -21,9 +21,25 @@ public class AdServiceImpl implements AdService{
 
 	@Override
 	public Advertisement save(Advertisement advertisement) { 
+		if(advertisement.getDescription() == null) {
+			throw new IllegalArgumentException("The description must exist");
+		}
+		if(advertisement.getDimensions() == null) {
+			throw new IllegalArgumentException("The dimensions must exist");
+
+		}
+		if(advertisement.getName() == null) {
+			throw new IllegalArgumentException("The name must exist");
+		}
+		if(advertisement.getPrice()==null) {
+			throw new IllegalArgumentException("The price must exist");
+		}
+		if(advertisement.getStyles() == null) {
+			throw new IllegalArgumentException("The styles must exist");
+		}
+		if(advertisement.getSpecification() == null) {
+			throw new IllegalArgumentException("The specification must exist");
+		}
         return adRepo.save(advertisement);
 	}
-	
-
-
 }
