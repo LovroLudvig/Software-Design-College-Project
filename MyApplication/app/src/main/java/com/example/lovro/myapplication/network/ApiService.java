1842cadcedac957a17963c838ce11e924a0f17ee
ledeni56 <lovro.ludvig@gmail.com>
2019-01-07 17:21:37 +0100

    resolving conflicts

--- MyApplication/app/src/main/java/com/example/lovro/myapplication/network/ApiService.java
+++ MyApplication/app/src/main/java/com/example/lovro/myapplication/network/ApiService.java
@@ -14,10 +14,9 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
-<<<<<<< Updated upstream
+
 import retrofit2.http.Query;
-=======
->>>>>>> Stashed changes
+
 
 public interface ApiService {
 
@@ -30,14 +29,14 @@ public interface ApiService {
     @GET("/advertisement/all")
     Call<List<Offer>> getAllOffers(@Header("Authorization") String auth);
 
-<<<<<<< Updated upstream
+
     @POST("/orders/add/{advertisementId}")
     Call<Order> orderOffer(@Path("advertisementId") String offerId, @Query("styleId") String styleId, @Query("dimensionId") String dimensionId, @Body User user);
-=======
+
     @GET("/user/{username}")
     Call<UserProfile> getUserByUsername(@Header("Authorization") String auth, @Path("username") String username);
 
     @POST("/user/update")
     Call<ResponseBody> updateUser(@Header("Authorization") String auth, @Body User user);
->>>>>>> Stashed changes
+
 }
