be3f4ee7b7feae73683de5fba43e949ec7338fd6
ledeni56 <lovro.ludvig@gmail.com>
2019-01-16 15:01:54 +0100

    removing unused res

--- MyApplication/app/src/main/java/com/example/lovro/myapplication/activities/OfferDetailsActivity.java
+++ MyApplication/app/src/main/java/com/example/lovro/myapplication/activities/OfferDetailsActivity.java
@@ -204,7 +204,7 @@ public class OfferDetailsActivity extends BasicActivity {
                         }
                     }
                 });
-                alertDialog.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.cancel();
